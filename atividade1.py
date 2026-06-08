fcb292c248cb918579786b3c112a45b3b20f7c2a
marentropico <marcosfigueroa.br@gmail.com>
2026-06-08 19:07:01 -0300

    Resultados da Atividade 1

--- atividade1.py
+++ atividade1.py
@@ -1,35 +1,26 @@
 # Exercício 1
-
 nota = 10
-
 print(nota <= 10)
 
 # Exercício 2
-
 nota = 6
 faltas = 4
-
 print((nota <= 6) and (faltas <= 3))
 
 # Exercício 3
-
 convidados = 3
 fumante = False
-
 print((convidados > 4) or (fumante == True))
 
 # Exercício 4
-
 dia = "qua"
 print((dia == "sab") or (dia == "dom"))
 
 # Exercício 5
-
 feriado = True
 print(not (feriado == False))
 
 # Exercício 6
-
 dia = "ter"
 feriado = False
 print((dia == "seg") or not (feriado == False))
